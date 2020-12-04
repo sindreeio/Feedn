1a6334e9275511487961b46b5f3b11de2819d67e
Magwest1 <magnus.olstad@gmail.com>
2020-12-04 11:41:23 +0100

    Gets params from url

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import Btn from '../../components/buttons/materialDesignFilledButton';
+import './feed.css';
 import {firebaseAuth} from '../../database/FirebaseConfig';
 import Navbar from '../../components/navbar/navbar.js';
-import {Redirect} from 'react-router-dom';
+import {Redirect, useParams} from 'react-router-dom';
+
 
 
 function Feed(){
+    let {id} = useParams()
 
     return(
         <div>
-            
             <Navbar/>
+            <div className="center">
+                <div className="f_header">
+                    {id[0]}
+                </div>
+            </div>
         </div>
     )
 }
