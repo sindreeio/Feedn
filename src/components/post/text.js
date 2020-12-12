5f4619dd7f0222009f0cc13b450acd789daf0b22
Magwest1 <magnus.olstad@gmail.com>
2020-12-12 14:43:56 +0100

    Returns null if text is empty

--- src/components/post/text.js
+++ src/components/post/text.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import './post.css';
+
 function Text(props) {
-    return (
-        <div className="text">
-            {props.text}
-        </div>
-    );
+    if (props.text) {
+        return (
+            <div className="text">
+                {props.text}
+            </div>
+        );
+    } else {
+        return null;
+    }
 }
+
+export default Text;
