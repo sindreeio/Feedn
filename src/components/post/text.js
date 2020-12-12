34da522bb8bb3f16327c024eb9c0e808e32da0e5
Magwest1 <magnus.olstad@gmail.com>
2020-12-12 14:01:38 +0100

    Displays text

--- src/components/post/text.js
+++ src/components/post/text.js
@@ -1,1 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import './post.css';
+function Text(props) {
+    return (
+        <div className="text">
+            {props.text}
+        </div>
+    );
+}
