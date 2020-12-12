4287c8ed848454e016abd71d252e36210616cb61
Magwest1 <magnus.olstad@gmail.com>
2020-12-12 13:17:56 +0100

    Prepares for firebase

---
+++ src/functions/server/getPosts.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { db, firebaseAuth } from '../../database/FirebaseConfig.js';
+
+export const getPosts = async (user, feedId) => {
+    db.collection();
+    return posts;
+
+}
