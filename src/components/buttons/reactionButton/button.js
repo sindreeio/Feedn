730c2e0966f7893646c90540a2d1b9e2bd6db2b7
= <=>
2020-12-17 16:29:03 +0100

    fixes reaction numbers

--- src/components/buttons/reactionButton/button.js
+++ src/components/buttons/reactionButton/button.js
@@ -7,14 +7,14 @@ function Button(props){
     if(props.reacted){
         return(
             <div class="standardButton" style={{backgroundColor: '#1ABDA5', color:' #eeeeee'}} onClick={props.action}>
-            {props.text} {props.number}
+            {props.text} <div className="number">{props.number}</div>
             </div>
         )
     }
     else{
         return(
             <div class="standardButton" onClick={props.action}>
-            <div>{props.text}</div> {(props.number > 0)  ? <div>{props.number}</div> : null }
+            <div>{props.text}</div> {(props.number > 0)  ? <div className="number">{props.number}</div> : null }
             </div>
         )
     }
